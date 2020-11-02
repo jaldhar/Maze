e9cf538ea3530aadb205d818c51c9234e6226e9e
Jaldhar H. Vyas <jaldhar@braincells.com>
2020-11-02 15:55:12 -0500

    Remove extra stuff from window title.

--- include/olcPixelGameEngine.h
+++ include/olcPixelGameEngine.h
@@ -2931,7 +2931,7 @@ namespace olc
 		{
 			nLastFPS = nFrameCount;
 			fFrameTimer -= 1.0f;
-			std::string sTitle = "OneLoneCoder.com - Pixel Game Engine - " + sAppName + " - FPS: " + std::to_string(nFrameCount);
+			std::string sTitle = sAppName;
 			platform->SetWindowTitle(sTitle);
 			nFrameCount = 0;
 		}
